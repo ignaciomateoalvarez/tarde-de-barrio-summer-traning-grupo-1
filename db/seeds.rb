4234b80fee67a8c7ba20923ca0ab31336ba1d130
gianlucasgalati <gianlucasgalati11@gmail.com>
2023-02-02 14:46:06 +0000

    Arreglo del aplication.css, modificaciones de divs por section, div por nav y...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do 
+  first_name= Faker::Name.first_name
+  last_name= Faker::Name.last_name
+  full_name= first_name+last_name
+  User.create!(first_name: first_name, last_name: last_name, mail: Faker::Internet.email(name: full_name)) 
+end
+
+p "Creadas #{User.count} usuarios"
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:mail) }
+  end
+
+  describe 'Uniqueness Validation' do
+    it { should validate_uniqueness_of(:mail) }
+  end
+
+  describe 'First Name Format' do
+    it { should allow_value("Julián D'Feregotti").for(:first_name) }
+    it { should allow_value('Gaston Núñez').for(:first_name) }
+    it { should_not allow_value('Pepe1234').for(:first_name) }
+    it { should_not allow_value('R@ul Perez').for(:first_name) }
+  end
+
+  describe 'Last Name Format' do
+    it { should allow_value('Perez Gonzales').for(:last_name) }
+    it { should allow_value('Perez Gonzales').for(:last_name) }
+    it { should_not allow_value('Acuña Fulanito12').for(:last_name) }
+    it { should_not allow_value('Gomez!').for(:last_name) }
+  end
+
+  describe 'Email Format' do
+    it { should allow_value('test@gmail.com').for(:mail) }
+  end
+end
