c127c24904bffa06169695018e7e69ecc2f7d674
gianlucasgalati <gianlucasgalati11@gmail.com>
2023-02-10 18:05:59 +0000

    Modificacion Faltantes

--- db/migrate/20230208132657_add_deactivated_to_users.rb
+++ db/migrate/20230208132657_add_deactivated_to_users.rb
@@ -1,5 +1,5 @@
 class AddDeactivatedToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :active, :boolean
+    add_column :users, :active, :boolean, default: true
   end
 end
