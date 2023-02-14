131760ea25ce1c72d4f585b46c9fe3e16f172dd9
CamilaTisera <camilanoemitisera@gmail.com>
2023-02-14 11:15:04 -0300

    Correciones

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ApplicationRecord
   validates :lastname, presence: true, format: { with: /\A[a-zA-Z\ñÑ\u00C0-\u017F\']+[\s?[a-zA-Z\ñÑ\u00C0-\u017F\']*]*\z/ }
   validates :address, presence: true
   validates :school_level, presence: true
-  validates :goes_to_school, presence: true
+  validates :goes_to_school, inclusion: { in: [true, false] }
 
   def calculate_age
     Time.now.year - birth.year
