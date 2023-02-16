d79f7104e901854cb5c7f1c08ff1eeeb8425cde0
CamilaTisera <camilanoemitisera@gmail.com>
2023-02-16 10:48:47 -0300

    Correciones

--- app/presenters/student_presenter.rb
+++ app/presenters/student_presenter.rb
@@ -3,14 +3,12 @@ class StudentPresenter
   attr_reader :students
   attr_reader :params
 
-  # su initialize deberia recibir los params del controller tambien asi que con un simple cambio quedaria
-  def initialize(params, students = []) # quizas es medio chancho... pero le doy un default value a students asi pueden usar el presenter en el show
-    @params = params
+  def initialize(params, students = []) 
     @students = students
   end
 
   def student
-    Student.find_by(id: @params[:id]).decorate #no estoy seguro si era asi el metodo que tenian pero era algo similar
+    Student.find_by(id: @params[:id]).decorate
   end
 
   def decorated_students
