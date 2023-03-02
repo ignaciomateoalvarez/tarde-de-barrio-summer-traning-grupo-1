f52ded91e9cdea6ae13ed482f571b803a56807e7
carlaibelc <carlaibelc@gmail.com>
2023-03-02 22:11:57 +0000

    Feature: emojis

---
+++ app/javascript/controllers/emoji_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import { createPopup } from "@picmo/popup-picker";
+
+// Connects to data-controller="emoji-picker"
+export default class extends Controller {
+  static targets = ['body', 'pickerContainer', 'button']
+
+  connect() {
+    
+    this.picker = createPopup({
+      rootElement: this.pickerContainerTarget, //modal
+    },{
+      triggerElement: this.buttonTarget, //boton carita
+      referenceElement: this.buttonTarget, //boton carita
+      position: 'bottom-start', 
+    });
+    
+    this.picker.addEventListener('emoji:select', (event) => { 
+      this.bodyTarget.value += event.emoji;
+    });
+  }
+
+  open() {
+    this.picker.toggle();
+  }
+}
